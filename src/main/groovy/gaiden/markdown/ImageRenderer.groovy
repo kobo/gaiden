2dc4268de666014285371db426ad94328bdf97e8
Hideki IGARASHI <hideki.develop@gmail.com>
2013-09-27 16:14:25 +0900

    Add feature to output a warning message when a image file doesn't exist

--- src/main/groovy/gaiden/markdown/ImageRenderer.groovy
+++ src/main/groovy/gaiden/markdown/ImageRenderer.groovy
@@ -16,6 +16,7 @@
 
 package gaiden.markdown
 
+import gaiden.Holders
 import gaiden.util.FileUtils
 
 /**
@@ -27,9 +28,11 @@ import gaiden.util.FileUtils
 class ImageRenderer {
 
     private String outputPagePath
+    private File staticDirectoryFile
 
-    ImageRenderer(String outputPagePath) {
+    ImageRenderer(String outputPagePath, File staticDirectoryFile = Holders.config.staticDirectoryFile) {
         this.outputPagePath = outputPagePath
+        this.staticDirectoryFile = staticDirectoryFile
     }
 
     /**
@@ -43,21 +46,27 @@ class ImageRenderer {
     /**
      * Returns a rendering object which has a image path replaced with an appropriate relative path.
      * <p>
-     * An original image path is used without a replacement if it doesn't start with a slash.
+     * An original image path is used without a replacement if it doesn't start with a slash or it is URL.
+     * <p>
+     * Also, when a image file doesn't exist, outputs a warning message.
      *
-     * @param url the path of image
+     * @param path the path of image
      * @param alt the alternate text
      * @return {@link ImageElement}'s instance
      */
-    ImageElement render(String url, String alt) {
-        if (!url.startsWith("/")) {
-            return new ImageElement(src: url, alt: alt)
+    ImageElement render(String path, String alt) {
+        if (path =~ /^\w+:\/\//) {
+            return new ImageElement(src: path, alt: alt)
         }
 
-        def outputPageFile = new File("/", outputPagePath)
-        def resourceFile = new File("/", url)
-        def src = FileUtils.getRelativePathForFileToFile(outputPageFile, resourceFile)
+        def outputPageFile = new File(staticDirectoryFile, outputPagePath)
+        def resourceFile = (path.startsWith("/")) ? new File(staticDirectoryFile, path) : new File(outputPageFile.parentFile, path)
 
+        if (!resourceFile.exists()) {
+            System.err.println("WARNING: " + Holders.getMessage("image.reference.not.exists.message", [path, outputPagePath]))
+        }
+
+        def src = (path.startsWith("/")) ? FileUtils.getRelativePathForFileToFile(outputPageFile, resourceFile) : path
         new ImageElement(src: src, alt: alt)
     }
 
--- src/test/groovy/gaiden/markdown/ImageRendererSpec.groovy
+++ src/test/groovy/gaiden/markdown/ImageRendererSpec.groovy
@@ -16,23 +16,44 @@
 
 package gaiden.markdown
 
+import gaiden.Holders
+import gaiden.message.MessageSource
 import spock.lang.Specification
 
 class ImageRendererSpec extends Specification {
 
+    def savedSystemErr
+
+    def setup() {
+        savedSystemErr = System.err
+
+        Holders.messageSource = new MessageSource()
+    }
+
+    def cleanup() {
+        System.err = savedSystemErr
+    }
+
     def "'render' should return a rendering object which has replaced image path"() {
         setup:
-        def renderer = new ImageRenderer(pagePath)
+        def renderer = new ImageRenderer(pagePath, new File("non-existent-static"))
+
+        and:
+        def printStream = Mock(PrintStream)
+        System.err = printStream
 
         when:
-        def rendering = renderer.render(imageUrl, "TEST_ALT")
+        def rendering = renderer.render(imagePath, "TEST_ALT")
 
         then:
-        rendering.src == imageSrc
+        rendering.src == renderingSrc
         rendering.alt == "TEST_ALT"
 
+        and: "output a warning message when a image file doesn't exist"
+        1 * printStream.println("WARNING: '${imagePath}' in the '${pagePath}' refers to non-existent file")
+
         where:
-        pagePath         | imageUrl         | imageSrc
+        pagePath         | imagePath        | renderingSrc
         "test.html"      | "/img/test.png"  | "img/test.png"
         "test.html"      | "/test.png"      | "test.png"
         "path/test.html" | "/img/test.png"  | "../img/test.png"
@@ -42,7 +63,7 @@ class ImageRendererSpec extends Specification {
 
     def "'render' should not replace an image path if doesn't start with a slash"() {
         setup:
-        def renderer = new ImageRenderer("test.html")
+        def renderer = new ImageRenderer("test.html", new File("test/resources/static-files"))
 
         when:
         def rendering = renderer.render("img/test.png", "TEST_ALT")
@@ -50,6 +71,57 @@ class ImageRendererSpec extends Specification {
         then:
         rendering.src == "img/test.png"
         rendering.alt == "TEST_ALT"
+    }
+
+    def "'render' should not output a warning message when a image file exists"() {
+        setup:
+        def renderer = new ImageRenderer("test.html", new File("src/test/resources/static-files"))
+
+        and:
+        def printStream = Mock(PrintStream)
+        System.err = printStream
+
+        when:
+        def rendering = renderer.render(imagePath, "TEST_ALT")
+
+        then:
+        rendering.src == renderingSrc
+        rendering.alt == "TEST_ALT"
+
+        and:
+        0 * printStream.println(_)
+
+        where:
+        imagePath           | renderingSrc
+        "/images/dummy.png" | "images/dummy.png"
+        "images/dummy.png"  | "images/dummy.png"
+    }
+
+    def "'render' should not output a warning message when a image path is URL"() {
+        setup:
+        def renderer = new ImageRenderer("test.html", new File("src/test/resources/static-files"))
+
+        and:
+        def printStream = Mock(PrintStream)
+        System.err = printStream
+
+        when:
+        def rendering = renderer.render(imagePath, "TEST_ALT")
+
+        then:
+        rendering.src == imagePath
+        rendering.alt == "TEST_ALT"
+
+        and:
+        0 * printStream.println(_)
+
+        where:
+        imagePath << [
+            "http://www.example.com/test.png",
+            "https://www.example.com/test.png",
+            "ftp://www.example.com/test.png",
+            "file:///path/to/test.png",
+        ]
     }
 
 }
