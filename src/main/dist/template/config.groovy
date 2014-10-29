64ee6618138b8987dbe9faf9be2554c235a670bf
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2014-10-29 16:02:23 +0900

    Update default cofig.groovy template

--- src/main/dist/template/config.groovy
+++ src/main/dist/template/config.groovy
@@ -4,29 +4,8 @@
 // The version of this document
 //version = ""
 
-// The title of TOC
-//tocTitle = "Table of contents"
-
-// The path of template file
-//templateFilePath = "templates/layout.html"
-
-// The path of TOC file
-//tocFilePath = "pages/toc.groovy"
-
-// The path of TOC output file
-//tocOutputFilePath = "toc.html"
-
 // The path of source files directory
 //sourceDirectoryPath = "."
 
-// The path of static files directory
-//staticDirectoryPath = "static"
-
 // The path of directory to be outputted a document
-//outputDirectoryPath = "build/html"
-
-// The encoding of input Markdown resource
-//inputEncoding = "UTF-8"
-
-// The encoding of output document
-//outputEncoding = "UTF-8"
+//outputDirectoryPath = "build"
